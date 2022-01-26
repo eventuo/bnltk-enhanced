172fd74e7b5e04dc3cac4a64891472597b9535e4
eventuo <eventuoto@proton.me>
2022-01-26 14:38:03 +0000

    Typo in comment.

--- pos_tagger/bn_pos_tagger.py
+++ pos_tagger/bn_pos_tagger.py
@@ -7,4 +7,6 @@
 
 import string
 import numpy as np
-from keras.models im
+from keras.models import load_model
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.preprocessing import L
