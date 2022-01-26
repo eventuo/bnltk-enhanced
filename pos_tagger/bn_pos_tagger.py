a35dd166177d455a6e0889e8cfee3122c4f4f839
eventuo <eventuoto@proton.me>
2022-01-26 13:19:19 +0000

    Removed some unused files.

--- pos_tagger/bn_pos_tagger.py
+++ pos_tagger/bn_pos_tagger.py
@@ -1,4 +1,10 @@
 # Bangla Natural Language Toolkit: Parts of Speech Tagger
 #
 # Copyright (C) 2019 BNLTK Project
-# Author: Ashraf Hossain <asr
+# Author: Ashraf Hossain <asrafhossain197@gmail.com>
+
+
+
+import string
+import numpy as np
+from keras.models im
