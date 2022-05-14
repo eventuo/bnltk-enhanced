0a6850ea925ac70ebcb8ad47b427a721e45cc510
eventuo <eventuoto@proton.me>
2022-05-14 11:36:08 +0000

    add guard

--- pos_tagger/trained_model.py
+++ pos_tagger/trained_model.py
@@ -211,4 +211,9 @@ class BanglaPosTagger:
 	    Split tagged sentences to X and y datasets and append some basic features.
 
 	    :param tagged_sentences: a list of POS tagged sentences
-	    :param tagged_sente
+	    :param tagged_sentences: list of list of tuples (term_i, tag_i)
+	    :return: 
+	    """
+	    X = []
+	    
+	    #pr
